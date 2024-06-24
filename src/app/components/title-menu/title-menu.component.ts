b285652d83731a6e57bfa093449871547e29bae8
Flavio Milani <frmilanij@gmail.com>
2024-06-24 07:43:49 -0300

    title-menu created

--- src/app/components/title-menu/title-menu.component.ts
+++ src/app/components/title-menu/title-menu.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TitleMenuComponent implements OnInit {
 
+  title: string = "BLOG DA MARIA";
+
   constructor() { }
 
   ngOnInit(): void {
