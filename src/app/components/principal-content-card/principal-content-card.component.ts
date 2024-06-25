87728509752a9ffa861510794f6bd40817eab24f
Flavio Milani <frmilanij@gmail.com>
2024-06-25 01:01:43 -0300

    principal card created

--- src/app/components/principal-content-card/principal-content-card.component.ts
+++ src/app/components/principal-content-card/principal-content-card.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./principal-content-card.component.css']
 })
 export class PrincipalContentCardComponent implements OnInit {
+  image: string = "../../../assets/images/retrato-de-um-astronauta-em-fato-espacial-fazendo-uma-atividade-comum-ao-ar-livre.jpg";
+  subject: string = "Astrology";
+  title: string = "New planet is being studied with the possibility of water";
 
   constructor() { }
 
